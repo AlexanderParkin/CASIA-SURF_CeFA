47789850c75076d863acbaf6f043191442448e59
a.parkin <a.parkin@visionlabs.ru>
2020-03-04 08:55:38 +0300

    Remove random from test preprocess, Reproducible process simplification

--- rgb_track/compile_submit_file.py
+++ rgb_track/compile_submit_file.py
@@ -5,4 +5,4 @@ if __name__ == '__main__':
     for idx in range(1, 4):
         df = pd.read_csv(f'experiment_tests/protocol_4_{idx}/rgb_track/exp1_protocol4_{idx}/TestFileLogger/output_4.csv')
         submit_file_df = submit_file_df.append(df[['video_id', 'output_score']], ignore_index=True)
-    submit_file_df.to_csv('./submit_file.txt', index=False, header=False)
+    submit_file_df.to_csv('./submit_file.txt', index=False, header=False, sep=' ')
